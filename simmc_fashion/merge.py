3423cad22ca86ccf7f5685b6ab9a9677fd460174
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-02 15:07:06 +0800

    Update merge.py

--- simmc_fashion/merge.py
+++ simmc_fashion/merge.py
@@ -1,11 +1,15 @@
+# If I want to use this this needs to be editted
+
 import json
-with open('ubuntu_test_subtask_1.json') as infile:
+with open('fashion_devtest_dials.json') as infile:
     data = json.load(infile)
 
-with open('ubuntu_responses_subtask_1.tsv') as infile:
-    for line, content in zip(infile, data):
-        _, candidate_id, utterance = line.split('\t')
-        content['options-for-correct-answers'] = [{'candidate-id':candidate_id, 'utterance':utterance}]
+with open('fashion_devtest_dials_retrieval_candidates,json') as infile:
+    responses = json.load(infile)
+    
+for line, content in zip(infile, data):
+    _, candidate_id, utterance = line.split('\t')
+    content['options-for-correct-answers'] = [{'candidate-id':candidate_id, 'utterance':utterance}]
 
 with open('test.json', 'w') as outfile:
     json.dump(data, outfile, indent=4)
