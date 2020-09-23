06dbb098a8cfae4d8a20e57333b562504961240e
chorseng <39493602+chorseng@users.noreply.github.com>
2020-09-24 01:43:46 +0800

    Update run_bart.py

--- run_bart.py
+++ run_bart.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     )
     if args.fp16:
         try:
-            from apex.apex import amp
+            from apex import amp
         except ImportError:
             raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
         model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
